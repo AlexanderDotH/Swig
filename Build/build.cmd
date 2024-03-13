97b59421a0c64a5645aa1bab5d112899380f20e0
Alex <alexander.heuschkel@gmail.com>
2024-03-13 01:47:06 +0100

    chore(code): Setup

--- Build/build.cmd
+++ Build/build.cmd
@@ -13,7 +13,7 @@ dotnet restore %PROJECT_FILE%
 echo "Publishing..."
 dotnet publish %PROJECT_FILE% -c Release -r osx-x64 /p:PublishReadyToRun=true /p:PublishTrimmed=true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o %APP_NAME%\OSX
 dotnet publish %PROJECT_FILE% -c Release -r win-x64 /p:PublishReadyToRun=true /p:PublishTrimmed=true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o %APP_NAME%\Windows
-dotnet publish %PROJECT_FILE% -c Release -r win-x86 /p:PublishReadyToRun=true /p:PublishTrimmed=true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o %APP_NAME%\Windows
+dotnet publish %PROJECT_FILE% -c Release -r win-x64 /p:PublishReadyToRun=true /p:PublishTrimmed=true --self-contained true -o %APP_NAME%\Windows\Build
 dotnet publish %PROJECT_FILE% -c Release -r linux-x64 /p:PublishReadyToRun=true /p:PublishTrimmed=true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o %APP_NAME%\Linux
 
 echo "Build complete!"
